10acaaad8a4d08a6c41156c5a84942f4adaea34e
lizunlong <lizunlong@gmail.com>
2026-02-03 21:34:45 +0800

    xcode files

---
+++ app/DIYTypeless/DIYTypelessTests/DIYTypelessTests.swift
@@ -0,0 +1,17 @@
+//
+//  DIYTypelessTests.swift
+//  DIYTypelessTests
+//
+//  Created by lizunlong on 2026/1/30.
+//
+
+import Testing
+@testable import DIYTypeless
+
+struct DIYTypelessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
